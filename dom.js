88838f9a58c89aab58bf4e30c16611576709022b
JuanFelipePena <47402793+JuanFelipePena@users.noreply.github.com>
2021-09-27 23:40:20 -0500

    Lo que se pudo

--- dom.js
+++ dom.js
@@ -1,7 +1,57 @@
-fetch('https://gist.githubusercontent.com/josejbocanegra/b1873c6b7e732144355bb1627b6895ed/raw/d91df4c8093c23c41dce6292d5c1ffce0f01a68b/newDatalog.json')
-  .then((response) => {
-    return response.json();
+const url = "https://gist.githubusercontent.com/josejbocanegra/b1873c6b7e732144355bb1627b6895ed/raw/d91df4c8093c23c41dce6292d5c1ffce0f01a68b/newDatalog.json"
+
+let datos;
+
+fetch(url).then(res=>res.json()).then(res=>{
+
+  datos = res
+    $('#table1').bootstrapTable({
+      data: datos,
+      columns: [{
+        title: '#'
+      },{
+        field: 'events',
+        title: 'Events'
+      }, {
+        field: 'squirrel',
+        title: 'Squirrel'
+      }]
+    })
+    
+
+  $(function() {
+    var data = []
+    for (let i = 0; i < datos.length; i++) {
+      let eventos = datos[i]["events"]
+      
+      for (let j = 0; j < eventos.length; j++) {
+       data.push(eventos[j]);
+      }
+    }
+    console.log(data.events)
+    $('#table2').bootstrapTable({data: data,
+      columns: [{
+        title: 'thjgfhjg' 
+      }]
+    })
   })
-  .then((myJson) => {
-    console.log(myJson);
-  });
+
+  })
+  
+  function runningFormatter(value, row, index) {
+    return index;
+}
+
+function rowStyle(row, index) {
+  if (row.squirrel) {
+    return {
+      classes: 'bg-red'
+    }
+  }
+  return {
+    css: {}
+  }
+}
+
+
+
